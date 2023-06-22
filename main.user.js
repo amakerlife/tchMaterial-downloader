d73fcd8dd2fa7df44adecff8b49c49dbde610b4c
Yuyang Gu <64350662+amakerlife@users.noreply.github.com>
2023-06-22 14:57:57 +0800

    v1.1.5

--- main.user.js
+++ main.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         国家中小学智慧教育平台电子课本下载
 // @namespace    https://github.com/amakerlife
-// @version      1.1.4
+// @version      1.1.5
 // @description  在国家中小学智慧教育平台网站中添加电子课本下载按钮，免登录下载电子课本
 // @author       Makerlife
 // @match        https://*.smartedu.cn/tchMaterial/detail*
@@ -12,68 +12,13 @@
 // @grant        none
 // ==/UserScript==
 
-const pdfUrlRegExp = /\/pdf.pdf$/;
-const originalFetch = window.fetch;
-window.fetch = function() {
-  return originalFetch.apply(this, arguments).then(response => {
-    if (pdfUrlRegExp.test(response.url)) {
-      console.log('PDF URL: ' + response.url);
-      localStorage.setItem('pdfUrl', response.url);
-    }
-    return response;
-  });
-};
-
-
-const downloadBtn = document.createElement('div');
-downloadBtn.classList.add('download-btn-wrapper');
-downloadBtn.innerHTML = '<a href="#" class="download-link"><span class="download-icon"><i class="fas fa-download"></i></span></a>';
-downloadBtn.style.position = 'fixed';
-downloadBtn.style.right = '20px';
-downloadBtn.style.bottom = '20px';
-downloadBtn.style.width = '50px';
-downloadBtn.style.height = '50px';
-downloadBtn.style.borderRadius = '25px';
-downloadBtn.style.backgroundColor = 'blue';
-downloadBtn.style.cursor = 'pointer';
-downloadBtn.style.color = '#ffffff';
-document.body.appendChild(downloadBtn);
-const faLink = document.createElement('link');
-faLink.rel = 'stylesheet';
-faLink.href = 'https://cdn.bootcdn.net/ajax/libs/font-awesome/5.15.3/css/all.min.css';
-document.head.appendChild(faLink);
-const style = document.createElement('style');
-style.textContent = `
-.download-link {
-  display: inline-block;
-  text-decoration: none;
-  color: #ffffff;
-  font-size: 0;
-  line-height: 50px;
+var url = window.location.href;
+var regex = /[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}/g;
+var match = regex.exec(url);
+if (match) {
+  var id = match[0];
+  var redirectUrl = `https://r3-ndr.ykt.cbern.com.cn/edu_product/esp/assets_document/${id}.pkg/pdf.pdf`;
+  window.location.assign(redirectUrl);
+} else {
+  console.log("No ContentID Found!");
 }
-
-.download-icon {
-  display: block;
-  font-size: 24px;
-  line-height: 50px;
-}
-
-.download-btn-wrapper {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-`;
-document.head.appendChild(style);
-
-
-downloadBtn.addEventListener('click', function() {
-  const pdfUrl = localStorage.getItem('pdfUrl');
-  if (pdfUrl) {
-    window.open(pdfUrl, '_blank');
-  } else {
-    console.log('No PDF URL');
-  }
-});
-
-document.body.appendChild(downloadBtn);
