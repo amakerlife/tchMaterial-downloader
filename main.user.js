a37eb1136148b0471f091df715d3bd850cd8c770
Makerlife <64350662+amakerlife@users.noreply.github.com>
2023-07-17 00:14:45 +0800

    Update main.user.js

--- main.user.js
+++ main.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         国家中小学智慧教育平台电子课本下载
 // @namespace    https://github.com/amakerlife
-// @version      1.1.6
+// @version      1.1.7
 // @description  在国家中小学智慧教育平台网站中添加电子课本下载按钮，免登录下载电子课本
 // @author       Makerlife
 // @match        https://*.smartedu.cn/tchMaterial/detail*
@@ -13,7 +13,7 @@
 // @compatible	 Chrome
 // @compatible	 Firefox
 // @compatible	 Edge
-// @compatible	 safari
+// @compatible	 Safari
 // ==/UserScript==
 
 var url = window.location.href;
