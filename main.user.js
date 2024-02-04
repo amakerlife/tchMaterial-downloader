a160ae6f30369af192d1a49d4fadc6ae600d8939
Makerlife <64350662+amakerlife@users.noreply.github.com>
2024-02-04 18:24:06 +0800

    Update main.user.js

--- main.user.js
+++ main.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         国家中小学智慧教育平台电子课本下载
 // @namespace    https://github.com/amakerlife
-// @version      1.1.7
+// @version      1.1.8
 // @description  在国家中小学智慧教育平台网站中添加电子课本下载按钮，免登录下载电子课本
 // @author       Makerlife
 // @match        https://*.smartedu.cn/tchMaterial/detail*
@@ -20,9 +20,12 @@ var url = window.location.href;
 var regex = /[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}/g;
 var match = regex.exec(url);
 if (match) {
+  var originalTitle = document.title;
+  document.title = `[Jumping] ${originalTitle}`;
   var id = match[0];
   var redirectUrl = `https://r3-ndr.ykt.cbern.com.cn/edu_product/esp/assets_document/${id}.pkg/pdf.pdf`;
   window.location.assign(redirectUrl);
-} else {
+}
+else {
   console.log("No ContentID Found!");
 }
