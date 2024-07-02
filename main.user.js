32041c0c657d2673475098e89c4ee6aed7bb6736
Makerlife <64350662+amakerlife@users.noreply.github.com>
2024-07-02 20:35:40 +0800

    Update main.user.js

--- main.user.js
+++ main.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         国家中小学智慧教育平台电子课本下载
 // @namespace    https://github.com/amakerlife
-// @version      1.1.8
+// @version      1.1.9
 // @description  在国家中小学智慧教育平台网站中添加电子课本下载按钮，免登录下载电子课本
 // @author       Makerlife
 // @match        https://*.smartedu.cn/tchMaterial/detail*
@@ -10,10 +10,10 @@
 // @icon         https://basic.smartedu.cn/favicon.ico
 // @license      MIT
 // @grant        none
-// @compatible	 Chrome
-// @compatible	 Firefox
-// @compatible	 Edge
-// @compatible	 Safari
+// @compatible   Chrome
+// @compatible   Firefox
+// @compatible   Edge
+// @compatible   Safari
 // ==/UserScript==
 
 var url = window.location.href;
@@ -21,11 +21,11 @@ var regex = /[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}/g;
 var match = regex.exec(url);
 if (match) {
   var originalTitle = document.title;
-  document.title = `[Jumping] ${originalTitle}`;
+  document.title = `[Redirecting] ${originalTitle}`;
   var id = match[0];
+  console.log(`ContentID: ${id}`);
   var redirectUrl = `https://r3-ndr.ykt.cbern.com.cn/edu_product/esp/assets_document/${id}.pkg/pdf.pdf`;
   window.location.assign(redirectUrl);
-}
-else {
+} else {
   console.log("No ContentID Found!");
 }
